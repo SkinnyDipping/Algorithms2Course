ad05ac1fcf6c6372d068a5f03f826419977bd96b
Szal <spam_szala@interia.pl>
2016-06-18 12:24:21 +0200

    WordNet v2

--- Week1/WordNet/src/WordNet.java
+++ Week1/WordNet/src/WordNet.java
@@ -6,67 +6,92 @@ public class WordNet {
 
     private static final int MAX_SIZE = 20;
 
-    private SAP sap;
-    private Digraph hypernyms_graph;
-    private int[] ids;
+    private SAP shortestAncestralPath;
+    private Digraph hypernymsGraph;
+    private SeparateChainingHashST<String, Integer> nounsHashSet;
+    private SeparateChainingHashST<Integer, String> synsetsHashSet;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
+        if (synsets == null || hypernyms == null) throw new NullPointerException();
 
         // sysnets
-        In sysnetsIn=new In(synsets);
-
+        In synsetsIn = new In(synsets);
+        nounsHashSet = new SeparateChainingHashST<>();
+        synsetsHashSet = new SeparateChainingHashST<>();
+        while (synsetsIn.hasNextLine()) {
+            String line = synsetsIn.readLine();
+            Scanner sc = new Scanner(line);
+            sc.useDelimiter(",");
+            int synsetsId = sc.nextInt();
+            String synset = sc.next();
+            synsetsHashSet.put(synsetsId, synset);
+            String[] nouns = synset.split(" ");
+            for (String noun : nouns)
+                nounsHashSet.put(noun, synsetsId);
+        }
 
         // hypernyms
         In hyprnymsIn = new In(hypernyms);
-        hypernyms_graph = new Digraph(MAX_SIZE);
+        hypernymsGraph = new Digraph(MAX_SIZE);
         while (hyprnymsIn.hasNextLine()) {
             String line = hyprnymsIn.readLine();
             Scanner sc = new Scanner(line);
             sc.useDelimiter(",");
             int v = sc.nextInt();
             int w = sc.nextInt();
-            hypernyms_graph.addEdge(v, w);
+            hypernymsGraph.addEdge(v, w);
         }
-        sap = new SAP(hypernyms_graph);
+        shortestAncestralPath = new SAP(hypernymsGraph);
 
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return null;
+        return nounsHashSet.keys();
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-        return false;
+        if (word == null) throw new NullPointerException();
+        return nounsHashSet.contains(word);
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
-        return -1;
+        if (!isNoun(nounA) || !isNoun(nounB)) throw new IllegalArgumentException();
+        int v = nounsHashSet.get(nounA);
+        int w = nounsHashSet.get(nounB);
+        return shortestAncestralPath.length(v, w);
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
+        if (!isNoun(nounA) || !isNoun(nounB)) throw new IllegalArgumentException();
+
         return null;
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
         WordNet wn = new WordNet(args[0], args[1]);
-        while (!StdIn.isEmpty()) {
-            int v = StdIn.readInt();
-            int w = StdIn.readInt();
-            StdOut.printf("length=%d, anc=%d\n", wn.sap.length(v, w), wn.sap.ancestor(v, w));
+        for (char c = 'a'; c < 'z' + 1; c++) {
+            if (c == 'q') continue;
+            int id = wn.nounsHashSet.get(String.valueOf(c));
+            StdOut.printf("String: %s id: %d\n", c, id);
         }
+        String noun = "aa";
+        StdOut.printf("%s is noun: %b\n", noun, wn.isNoun(noun));
+        for (String s : wn.nouns()) {
+            StdOut.print(s + "\n");
+        }
+
+//        while (!StdIn.isEmpty()) {
+//            int v = StdIn.readInt();
+//            int w = StdIn.readInt();
+//            StdOut.printf("length=%d, anc=%d\n", wn.shortestAncestralPath.length(v, w), wn.shortestAncestralPath.ancestor(v, w));
+//        }
     }
 
-    private int getID(String noun) {
-        for (String s:nouns()){
-
-        }
-        return -1;
-    }
 }
